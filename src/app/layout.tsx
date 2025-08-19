1c3d1dcfbdbf0e2d25d8460a6c145deebf0cb99d
Irving Dinh <irving.dinh@gmail.com>
2025-08-19 13:20:12 +0700

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import { type Metadata } from 'next'
+
+import { Providers } from '@/app/providers'
+import { Layout } from '@/components/Layout'
+
+import '@/styles/tailwind.css'
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s — Irving Dinh',
+    default:
+      'Irving Dinh — Software engineer, designer, and amateur photographer',
+  },
+  description:
+    'I’m Irving, a software engineer and designer based in Ho Chi Minh City, Viet Nam.',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" className="h-full antialiased" suppressHydrationWarning>
+      <body className="flex h-full bg-zinc-50 dark:bg-black">
+        <Providers>
+          <div className="flex w-full">
+            <Layout>{children}</Layout>
+          </div>
+        </Providers>
+      </body>
+    </html>
+  )
+}
